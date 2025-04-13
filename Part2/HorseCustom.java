cf9feace6b23b70105b8390db06a9814b8eaf1fb
Lewis-T3A <ec24493@qmul.ac.uk>
2025-04-13 20:11:05 +0100

    Update HorseCustom.java

--- Part2/HorseCustom.java
+++ Part2/HorseCustom.java
@@ -8,6 +8,16 @@ public class HorseCustom {
         JPanel panel2 = new JPanel (new GridLayout(5,2,10,10));
         frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
+    //Horse Breed
+    JLabel horseBreed = new JLabel("Horse Breed");
+    JComboBox<String> breedChooser = new JComboBox<>(new String[]{"Arabian","Thoroughbred","Quarter Horse"});
 
+    //Coat Colour
+    JLabel horseColour = new JLabel ( "Coat Colour");
+    JComboBox<String> colourChooser = new JComboBox<>( new String[]{"Brown", "White","Black","Red","Golden","Spotted"});
+    //Symbol 
+    JLabel horseSymbol = new JLabel ( "Horse Symbol ");
+    JTextField symbolInput = new JTextField(1);
+    //Equipment and Accessories
+    
 }
