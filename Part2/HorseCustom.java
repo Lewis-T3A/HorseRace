937e0037e03dd3e37978fac3d87a07bdf8e91bf5
Lewis-T3A <ec24493@qmul.ac.uk>
2025-04-13 19:07:24 +0100

    Creating new JPanel and JFrame
    Creating the basic layout for my next gui panel

--- Part2/HorseCustom.java
+++ Part2/HorseCustom.java
@@ -1,5 +1,13 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 public class HorseCustom {
+    public static void main(String[] args)
+    {
+        JFrame frame2 = new JFrame ( "Horse Customisation");
+        frame2.setSize(600,400);
+        JPanel panel2 = new JPanel (new GridLayout(5,2,10,10));
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     
+
 }
