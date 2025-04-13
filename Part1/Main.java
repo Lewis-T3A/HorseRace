52f522248d3818fcc57d06ec816ee3c006754ea1
Lewis-T3A <ec24493@qmul.ac.uk>
2025-04-13 16:35:41 +0100

    Unnecesary main class
    I now am currently running my code in the GUI file, so the main class had no use and was only causing a file conflict with my prior main class.

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,1 +1,22 @@
+public class Main {
+    public static void main(String[] args) 
+    {
+        Horse horse1 = new Horse('A', "Jacob",0.6);
+        Horse horse2 = new Horse('B', "Carlos" ,0.6);
+        Horse horse3 = new Horse('C', "Melon",0.6);
 
+        Race myRace = new Race(5);
+        
+        myRace.addHorse(horse1, 1);
+        myRace.addHorse(horse2, 2);
+        myRace.addHorse(horse3, 3);
+        myRace.startRace();
+
+        Race myRace2 = new Race(5);
+
+        myRace2.addHorse(horse1,1);
+        myRace2.addHorse(horse2,2);
+        myRace2.addHorse(horse3,3);
+        myRace2.startRace();
+    }
+}
