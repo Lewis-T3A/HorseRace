2db57bc09af0e9d1e2f20742bccfd8a9bf299c75
Lewis-T3A <ec24493@qmul.ac.uk>
2025-04-14 00:26:53 +0100

    Preparation for Horse Arrays
    This is the preparation for allowing my GUI to get values for multiple horses, by creating the input boxes into arrays, which can be fed into my main program.

--- Part2/HorseCustom.java
+++ Part2/HorseCustom.java
@@ -1,41 +1,42 @@
 import java.awt.*;
 import javax.swing.*;
 public class HorseCustom {
-    public static void main(String[] args)
+    public HorseCustom(int laneCount)
     {
         JFrame frame2 = new JFrame ( "Horse Customisation");
         frame2.setSize(600,400);
         JPanel panel2 = new JPanel (new GridLayout(5,2,10,10));
         frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+        //Horse Breed
+        JLabel horseBreed = new JLabel("Horse Breed");
+        JComboBox<String> breedChooser = new JComboBox<>(new String[]{"Arabian","Thoroughbred","Quarter Horse"});
+
+        //Coat Colour
+        JLabel horseColour = new JLabel ( "Coat Colour");
+        JComboBox<String> colourChooser = new JComboBox<>( new String[]{"Brown", "White","Black","Red","Golden","Spotted"});
+        
+        //Symbol label and input
+        JLabel horseSymbol = new JLabel ( "Horse Symbol ");
+        JTextField symbolInput = new JTextField(1);
+        
+        //Equipment and Accessories
+        
+        //Saddle colour
+        JLabel saddleColour = new JLabel("Saddle:");
+        JComboBox<String> saddleChooser = new JComboBox<> (new String[]{"Black","White","Gold","Red","Blue"});
+
+        //HorseShoe type
+        JLabel horseShoeLabel = new JLabel ("HorseShoes:");
+        JComboBox<String> horseShoeChooser = new JComboBox<>(new String[]{"Light","Heavy","Regular"});
+
+        //Accessory
+
+        JLabel accessoryLabel = new JLabel("Accessory:");
+        JComboBox<String> accessoryChooser = new JComboBox<>(new String[]{"none","Hat","Blanket","Birdle"});
     }
-    //Horse Breed
-    JLabel horseBreed = new JLabel("Horse Breed");
-    JComboBox<String> breedChooser = new JComboBox<>(new String[]{"Arabian","Thoroughbred","Quarter Horse"});
-
-    //Coat Colour
-    JLabel horseColour = new JLabel ( "Coat Colour");
-    JComboBox<String> colourChooser = new JComboBox<>( new String[]{"Brown", "White","Black","Red","Golden","Spotted"});
-    
-    //Symbol label and input
-    JLabel horseSymbol = new JLabel ( "Horse Symbol ");
-    JTextField symbolInput = new JTextField(1);
-    
-    //Equipment and Accessories
-    
-    //Saddle colour
-    JLabel saddleColour = new JLabel("Saddle:");
-    JComboBox<String> saddleChooser = new JComboBox<> (new String[]{"Black","White","Gold","Red","Blue"});
-
-    //HorseShoe type
-    JLabel horseShoeLabel = new JLabel ("HorseShoes:");
-    JComboBox<String> horseShoeChooser = new JComboBox<>(new String[]{"Light","Heavy","Regular"});
-
-    //Accessory
-
-    JLabel accessoryLabel = new JLabel("Accessory:");
-    JComboBox<String> accessoryChooser = new JComboBox<>(new String[]{"none","Hat","Blanket","Birdle"});
 
     
-
 
 }
