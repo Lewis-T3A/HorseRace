3eae1e222ec866c64ee6be4930dd006d950bbcfe
Lewis-T3A <ec24493@qmul.ac.uk>
2025-04-14 01:02:31 +0100

    Fixing GUI appearance issues
    In the GUI i had before, all the buttons i wanted appeared, but in a disorganised fashion which made it almost impossible to set everything for the Horses, so in this version, i am trying an integrated grid inside a box layout.

--- Part2/HorseCustom.java
+++ Part2/HorseCustom.java
@@ -5,7 +5,8 @@ public class HorseCustom {
     {
         JFrame frame2 = new JFrame ( "Horse Customisation");
         frame2.setSize(600,400);
-        JPanel panel2 = new JPanel (new GridLayout(laneCount + 1,6,10,10));
+        JPanel panel2 = new JPanel ();
+        panel2.setLayout(new BoxLayout(panel2,BoxLayout.Y_AXIS));
         frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JComboBox<String>[]breedChooser = new JComboBox[laneCount];
@@ -17,47 +18,44 @@ public class HorseCustom {
 
         for ( int i = 0; i<laneCount;i++)
         {
+            JPanel panelrow = new JPanel(new GridLayout(2,6,10,10));
+            panelrow.add(new JLabel ("Breed:"));
+            panelrow.add(new JLabel (""));
+            panelrow.add(new JLabel (""));
+            panelrow.add(new JLabel (""));
+            panelrow.add(new JLabel (""));
+            panelrow.add(new JLabel (""));
+            
             //Horse Breed
-            JLabel horseBreed = new JLabel("Horse Breed");
+            
             breedChooser[i] = new JComboBox<>(new String[]{"Arabian","Thoroughbred","Quarter Horse"});
 
             //Coat Colour
-            JLabel horseColour = new JLabel ( "Coat Colour");
+            
             colourChooser[i] = new JComboBox<>( new String[]{"Brown", "White","Black","Red","Golden","Spotted"});
             
             //Symbol label and input
-            JLabel horseSymbol = new JLabel ( "Horse Symbol ");
+            
             symbolInput[i] = new JTextField(1);
             
             //Equipment and Accessories
             
             //Saddle colour
-            JLabel saddleColour = new JLabel("Saddle:");
+            
             saddleChooser[i] = new JComboBox<> (new String[]{"Black","White","Gold","Red","Blue"});
 
             //HorseShoe type
-            JLabel horseShoeLabel = new JLabel ("HorseShoes:");
+            
             horseshoeChooser[i] = new JComboBox<>(new String[]{"Light","Heavy","Regular"});
 
             //Accessory
 
-            JLabel accessoryLabel = new JLabel("Accessory:");
+            
             accessoryChooser[i] = new JComboBox<>(new String[]{"none","Hat","Blanket","Birdle"});
             
-            panel2.add(horseBreed);
-            panel2.add(breedChooser[i]);
-            panel2.add(horseColour);
-            panel2.add(colourChooser[i]);
-            panel2.add(horseSymbol);
-            panel2.add(symbolInput[i]);
-            panel2.add(saddleColour);
-            panel2.add(saddleChooser[i]);
-            panel2.add(horseShoeLabel);
-            panel2.add(horseshoeChooser[i]);
-            panel2.add(accessoryLabel);
-            panel2.add(accessoryChooser[i]);
+  
 
-        frame2.add(panel2);
+        panel2.add(panelrow);
         }
        
         frame2.add(panel2);
