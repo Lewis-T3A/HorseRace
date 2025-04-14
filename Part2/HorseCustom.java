114dcc569f9117e113bc4c5e1afe65b62443f2de
Lewis-T3A <ec24493@qmul.ac.uk>
2025-04-14 01:07:23 +0100

    Final panel additions to Horse Customisation
    These are the final panels that had to be added to the Horse Customisation panel

--- Part2/HorseCustom.java
+++ Part2/HorseCustom.java
@@ -4,7 +4,7 @@ public class HorseCustom {
     public HorseCustom(int laneCount)
     {
         JFrame frame2 = new JFrame ( "Horse Customisation");
-        frame2.setSize(600,400);
+        frame2.setSize(1000,600);
         JPanel panel2 = new JPanel ();
         panel2.setLayout(new BoxLayout(panel2,BoxLayout.Y_AXIS));
         frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,11 +20,11 @@ public class HorseCustom {
         {
             JPanel panelrow = new JPanel(new GridLayout(2,6,10,10));
             panelrow.add(new JLabel ("Breed:"));
-            panelrow.add(new JLabel (""));
-            panelrow.add(new JLabel (""));
-            panelrow.add(new JLabel (""));
-            panelrow.add(new JLabel (""));
-            panelrow.add(new JLabel (""));
+            panelrow.add(new JLabel ("Colour:"));
+            panelrow.add(new JLabel ("Symbol:"));
+            panelrow.add(new JLabel ("Saddle type:"));
+            panelrow.add(new JLabel ("Horseshoe type:"));
+            panelrow.add(new JLabel ("Accessory:"));
             
             //Horse Breed
             
@@ -39,7 +39,6 @@ public class HorseCustom {
             symbolInput[i] = new JTextField(1);
             
             //Equipment and Accessories
-            
             //Saddle colour
             
             saddleChooser[i] = new JComboBox<> (new String[]{"Black","White","Gold","Red","Blue"});
@@ -52,7 +51,13 @@ public class HorseCustom {
 
             
             accessoryChooser[i] = new JComboBox<>(new String[]{"none","Hat","Blanket","Birdle"});
-            
+           
+            panelrow.add(breedChooser[i]);
+            panelrow.add(colourChooser[i]);
+            panelrow.add(symbolInput[i]);
+            panelrow.add(saddleChooser[i]);
+            panelrow.add(horseshoeChooser[i]);
+            panelrow.add(accessoryChooser[i]);
   
 
         panel2.add(panelrow);
@@ -65,6 +70,6 @@ public class HorseCustom {
 
     
     public static void main(String[] args) {
-        new HorseCustom(3); // test with 3 horses
+        new HorseCustom(4); // test with 3 horses
     }
 }
