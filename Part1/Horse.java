c80271fa0a953b85bcfa757007a27b9467634463
Lewis-T3A <ec24493@qmul.ac.uk>
2025-04-22 14:21:39 +0100

    Updating the Author
    Changing comments around

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Lewis John Millin) 
+ * @version (2.0)
  */
 public class Horse
 {
